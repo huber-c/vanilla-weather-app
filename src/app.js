93dbd662d20cc4e66e1020dde771b6686e6939b5
Carina Huber <huber.carina2806@gmail.com>
2020-11-15 20:53:10 +0000

    Added weather icons from API

--- src/app.js
+++ src/app.js
@@ -21,16 +21,20 @@ function displayTemperatureDetails(response) {
     let windElement = document.querySelector("#wind");
     let humidityElement = document.querySelector("#humidity");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#weather-icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 
 let apiKey = "2ceada01b7fc7d0d21b702ddd8150d97";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Manchester&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperatureDetails);
