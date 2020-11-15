588a9f2060543214ee3563b57111eb127d629db1
Carina Huber <huber.carina2806@gmail.com>
2020-11-15 19:58:54 +0000

    Add Date - remove console.log

--- src/app.js
+++ src/app.js
@@ -1,20 +1,36 @@
-function displayTemperature(response) {
-    console.log(response.data);
+function formatDate(timestamp) {
+    //calculate the date
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperatureDetails(response) {
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let windElement = document.querySelector("#wind");
     let humidityElement = document.querySelector("#humidity");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
 let apiKey = "2ceada01b7fc7d0d21b702ddd8150d97";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Manchester&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperatureDetails);
